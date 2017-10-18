59f2870c71f675bcdcf94b8ccc40d1ffc03af219
Luis Benavides <luisbenan@gmail.com>
2017-10-17 22:51:42 -0400

    Add thinkin in react files

---
+++ thinking-in-react/solution.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const FilterableProductTable = React.createClass({
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+})
+
+export const SearchBar = React.createClass({
+  render() {
+    return (
+        <input/> // issue here, intructions are not clear
+    )
+  }
+})
